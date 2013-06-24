e5886ae878226d6757e6e7a7a1bcaa3d0b0cf198
Oswazteca <oswazteca@gmail.com>
2013-06-24 12:27:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OzPage::Application.config.secret_token = '09ee4866e03a90bcb229c6128d801bab6f39f6ac284c226e13583b457f2856752432ee91d602107eae5a1d875cb558b98d038e96bfafd8c947dd41fe1ec1faa7'
